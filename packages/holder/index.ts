0c60150ce44c804e7aa4f83175673434855c34fb
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-10-25 22:58:32 -0600

    generate KeyValuePair

--- packages/holder/index.ts
+++ packages/holder/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello via Bun!");
+import { generateKeyPair } from "crypto";
+
+function getKeyValuePair() {
+	const keyPair = generateKeyPair(
+		"rsa",
+		{
+			modulusLength: 4096,
+			publicKeyEncoding: {
+				type: "spki",
+				format: "pem",
+			},
+			privateKeyEncoding: {
+				type: "pkcs8",
+				format: "pem",
+				cipher: "aes-256-cbc",
+				passphrase: "super top secret",
+			},
+		},
+		(err, publicKey, privateKey) => {
+			// Handle errors and use the generated key pair.
+		},
+	);
+	return keyPair;
+}
