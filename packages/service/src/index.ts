c0f6d6b65c82f9131b5e661fb83aa3b7e7d9617a
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-10-28 19:05:04 -0600

    added token against replay attacks

--- packages/service/src/index.ts
+++ packages/service/src/index.ts
@@ -1,9 +1,23 @@
+import { randomBytes } from "crypto";
 import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-	return c.text("Hello Hono!");
+const nonceStore = new Set<string>();
+
+app.get("/nonce", (c) => {
+	const token = randomBytes(16).toString("hex");
+	const expiration = Date.now() + 5 * 60 * 1000; // 5 minutes from now
+
+	nonceStore.add(token);
+
+	setTimeout(() => {
+		nonceStore.delete(token);
+	}, expiration - Date.now());
+
+	console.log(`Token issued: ${token}`);
+
+	return c.json({ token });
 });
 
 export default app;
